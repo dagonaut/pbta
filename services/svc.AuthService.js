0a8f3c3351bf2393b57e1084d438570112e5a35c
RIGGS\paul <dagonaut@gmail.com>
2018-03-03 21:11:17 -0600

    refactor: moved some services into their own files.

---
+++ services/svc.AuthService.js
@@ -0,0 +1,118 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pbta_resources')
+        .factory('Auth', Auth);
+
+    Auth.$inject = ['Base64', '$http', '$cookies', '$rootScope', '$timeout', '$q'];
+    function Auth(Base64, $http, $cookies, $rootScope, $timeout, $q) {
+        //Private Properties
+		var api = 'http://16watt.com/dev/pbta/api/api.php/';
+		
+		// Set factory return object
+		var Auth = {};
+		
+		var user;
+		
+		Auth.setUser = function(aUser){
+			user = aUser;
+		};
+
+		Auth.checkUser = function(){
+			if(typeof $rootScope.userData !== 'undefined'){
+				return true;
+			} else {
+				//Not logged in, check for cookie
+				var userId = $cookies.getObject('id');
+				if (typeof userId !== 'undefined'){
+					//We have a cookie, let's log this sucka in
+					Auth.AutoLogin(userId, function(response){
+						console.log("Logging in " + $rootScope.userData.username + " from cookie");						
+					
+					});										
+					return true;
+				} else {
+					return false;
+				}				
+			}
+			/*
+			if(typeof user === "undefined"){
+				if(typeof $cookies.getObject('id') !== 'undefined'){
+					Auth.AutoLogin($cookies.getObject('id'));
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				Auth.AutoLogin($cookies.getObject('id'));
+				return true;
+			}
+			*/
+		}
+		
+		Auth.isLoggedIn = function(){
+			return(user) ? user : false;
+		};
+		
+		Auth.Login = function (username, password, callback) {
+			$http.get(api + 'tbl_Users/username/"' + username + '"').then(
+				function(response){						
+					response.success = (password === response.data.Password);
+					callback(response);
+					Auth.setUser(response.data.id);
+				},
+				function(error){
+					console.log(error);
+				}
+			);
+			/*
+			$http.post('./api.php/tbl_Users', { username: username, password: password })
+			.success(function (response) {
+				console.log("user success");
+				callback(response);
+			});
+			*/		
+		};
+
+		Auth.AutoLogin = function(userId, callback){
+            $http.get(api + 'tbl_Users/id/"' + userId + '"').then(
+                function(response){
+              		$rootScope.userData = response.data;
+                    Auth.setUser(response.data.id);
+                    response.success = true;
+                    callback(response);
+                },
+                function(error){
+                    console.log(error);
+                }
+            );
+		};
+		
+		Auth.SetCredentials = function (username, password, user) {
+			var authdata = Base64.encode(username + ':' + password);
+			
+			$rootScope.globals = {
+				currentUser: user
+			};
+			$rootScope.userData = user;
+			
+			$http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+			//$cookieStore.put('globals', $rootScope.globals);
+			var now = new Date(),
+			// this will set the expiration to 12 months
+			exp = new Date(now.getFullYear()+1, now.getMonth(), now.getDate());
+			$cookies.putObject('id', $rootScope.userData.id, {expires: exp});
+		};
+		
+		Auth.ClearCredentials = function () {
+			$rootScope.globals = {};
+			$rootScope.userData = {};
+			$cookies.remove('id');
+			$http.defaults.headers.common.Authorization = 'Basic ';
+		};
+		
+		return Auth;
+    }
+
+})();
---
+++ services/svc.UserService.js
@@ -0,0 +1,62 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pbta_resources')
+        .factory('UserService', UserService);
+
+    UserService.$inject = ['$http', '$cookies', '$rootScope', '$timeout', '$q'];
+    function UserService($http, $cookies, $rootScope, $timeout, $q) {
+        var api = 'http://16watt.com/dev/pbta/api/api.php/';
+		var table = 'tbl_Users/';
+		
+		var UserService = {};
+
+		UserService.GetAll = GetAll;
+        UserService.GetById = GetById;
+        UserService.GetByUsername = GetByUsername;
+		/*
+		UserService.Create = Create;
+        UserService.Update = Update;
+		UserService.Delete = Delete;
+		*/
+		return UserService;
+
+        function GetAll() {
+            return $http.get(api + table).then(handleSuccess, handleError('Error getting all users'));
+        }
+
+        function GetById(id) {
+            return $http.get(api + table + '/id/' + id).then(handleSuccess, handleError('Error getting user by id'));
+        }
+
+        function GetByUsername(username) {
+            return $http.get(api + table + '/username/' + username).then(handleSuccess, handleError('Error getting user by username'));
+        }
+		/*
+        function Create(user) {
+            return $http.post(api + table + user).then(handleSuccess, handleError('Error creating user'));
+        }
+
+        function Update(user) {
+            return $http.put(api + table + user.id, user).then(handleSuccess, handleError('Error updating user'));
+        }
+
+        function Delete(id) {
+            return $http.delete(api + table + id).then(handleSuccess, handleError('Error deleting user'));
+        }
+		*/
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
