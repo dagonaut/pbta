e493b6914b8b606c123990b67fcab515e0261b1c
Paul <dagonaut@gmail.com>
2018-07-18 20:48:55 -0500

    feature: added MoveService, added Class selector to Sprawl (still a "selected" bug), added sprawl link to wrapper (still not under Auth).

--- controllers/ctrl.Sprawl.js
+++ controllers/ctrl.Sprawl.js
@@ -5,8 +5,8 @@
         .module('pbta_resources')
         .controller('Sprawl', sprawl);
         
-        sprawl.$inject = ['$rootScope','$scope','$http','$q','$state','$stateParams','$location','$cookies', '$sce','Auth','UserService','ClockService'];
-        function sprawl($rootScope, $scope, $http, $q, $state, $stateParams, $location, $cookies, $sce, Auth, UserService, ClockService){
+        sprawl.$inject = ['$rootScope','$scope','$http','$q','$state','$stateParams','$location','$cookies', '$sce','Auth','UserService','ClockService','MoveService'];
+        function sprawl($rootScope, $scope, $http, $q, $state, $stateParams, $location, $cookies, $sce, Auth, UserService, ClockService, MoveService){
             //Private Properties
             var vm = this;
             var api = 'http://16watt.com/dev/pbta/api/api.php/';
@@ -15,6 +15,9 @@
             //Scope Properties
             vm.harm = ["","","","","",""]; // THe Harm array represents the 15,18,21,22,23,00 of the harm bar
             vm.directives = directives();
+            vm.classes = [{name:'driver'}, {name:'fixer'}, {name:'hacker'}, {name:'hunter'},{name:'infiltrator'},{name:'killer'},{name:'pusher'},{name:'reporter'},{name:'soldier'},{name:'tech'}];
+            vm.class = "driver";
+            vm.classMoves = getClassMoves(vm.class);
             
             //Scope Methods	
             vm.markHarm = markHarm;
@@ -24,6 +27,7 @@
             vm.clocks = getClocksByUserId(userId);
             vm.createClock = createClock;
             vm.deleteClock = deleteClock;
+            vm.getClassMoves = getClassMoves;
 
             init();
             
@@ -31,6 +35,7 @@
                 console.log(vm.clocks);
             }
 
+            // Clocks
             function createClock(){
                 var newClock = {
                     type: '',
@@ -53,7 +58,17 @@
                     vm.clocks = data;
                 });
             }
+
+            // Moves
+            function getClassMoves(c){
+                MoveService.GetByClass(c).then(function(data){
+                    vm.classMoves = data;
+                    console.log(vm.classMoves);
+
+                });
+            }
             
+            // Harm
             function markHarm(position){
                 clearHarm();
                 for(var i = 0; i < vm.harm.length; i++){
@@ -67,6 +82,7 @@
                 vm.harm = ["","","","","",""];  
             }
 
+            // Directives
             function directives(){
                 return "<ul style='width:300px;'><li>Illustrious: When your desire for fame draws unwanted attention to the mission, mark experience. </li> <li>Intimate: When you put your friend ________________ ahead of the mission, mark experience.</li>                <li>Rejected: When your former membership of _________________ hinders the mission, mark experience. </li>                <li>Vengeful: When you harm _______________ or their interests, mark experience. </li>                    </ul>";
                 
---
+++ services/svc.MoveService.js
@@ -0,0 +1,86 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pbta_resources')
+        .factory('MoveService', MoveService);
+
+    MoveService.$inject = ['$http', '$cookies', '$rootScope', '$timeout', '$q'];
+    function MoveService($http, $cookies, $rootScope, $timeout, $q) {
+        var api = 'http://16watt.com/dev/pbta/api/api.php/';
+		var table = 'tbl_Moves';
+		var config = {
+            method: 'GET',
+            url: api + table,
+            data: {}
+        }
+		var MoveService = {};
+
+		MoveService.GetAll = GetAll;
+        MoveService.GetById = GetById;
+        MoveService.GetByGameId = GetByGameId;
+        MoveService.GetByClass = GetByClass;
+        MoveService.GetByType = GetByType;
+        MoveService.GetByUserId = GetByUserId;
+		MoveService.Create = Create;
+        MoveService.Update = Update;
+		MoveService.Delete = Delete;
+		return MoveService;
+
+        function GetAll() {            
+            return $http(config).then(handleSuccess, handleError('Error getting all Moves'));
+        }
+
+        function GetById(id) {
+            config.url = config.url + '/id/' + id;
+            return $http(config).then(handleSuccess, handleError('Error getting Move by id'));
+        }
+
+        function GetByGameId(gameId) {
+            return $http.get(api + table + '/gameid/' + gameId).then(handleSuccess, handleError('Error getting Move by gameid'));
+        }
+
+        function GetByClass(characterClass) {
+            return $http.get(api + table + '/class/"' + characterClass + '"').then(handleSuccess, handleError('Error getting Move by characterClass'));
+        }
+
+        function GetByType(type) {
+            return $http.get(api + table + '/type/' + type).then(handleSuccess, handleError('Error getting Move by type'));
+        }
+
+        function GetByUserId(createdby) {
+            return $http.get(api + table + '/createdby/' + createdby).then(handleSuccess, handleError('Error getting Move by type'));            
+        }
+		
+        function Create(Move) {
+            config.method = "POST"
+            config.data = Move;
+            return $http(config).then(handleSuccess, handleError('Error creating Move'));
+        }
+
+        function Update(Move) {
+            config.method = "PUT"
+            config.data = Move;
+            return $http(config).then(handleSuccess, handleError('Error updating Move'));
+        }
+
+        function Delete(id) {
+            config.method = "DELETE";
+            config.url = config.url + id
+            return $http(config).then(handleSuccess, handleError('Error deleting Move'));
+        }
+		
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
