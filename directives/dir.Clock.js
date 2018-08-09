280b68114744571488c5830b3aa360039871e8e0
Paul <dagonaut@gmail.com>
2018-08-09 08:32:42 -0500

    f: still working on the clock directive

--- directives/dir.Clock.js
+++ directives/dir.Clock.js
@@ -8,10 +8,22 @@
         function clock(){
             return{
                 restrict: 'E',                
-                templateUrl: "/directives/dir.Clock.html",
+                templateUrl: "./directives/dir.Clock.html",
                 scope: {
-                    nombre: '='                    
-                }
+                    position: '=',
+                    nomnbre: '@'                    
+                },
+                controller: Controller,
+                controllerAs: 'vm',
+                bindToController: true
             };
+            Controller.$inject['$scope'];
+            function Controller($scope){
+                var vm = this;
+
+                vm.position = 4;
+                vm.nomnbre = 'Tama Tonga';
+                console.log(vm);
+            }
         }
     })();
